503932b383b62182f8de73cc1fc56ed6ebdeb400
Krystian Sz <sztadii@gmail.com>
2023-03-26 18:32:30 +0700

    Add debounce during searching albums

--- src/pages/HomePage/components/AlbumsSearchField/AlbumsSearchField.tsx
+++ src/pages/HomePage/components/AlbumsSearchField/AlbumsSearchField.tsx
@@ -1,16 +1,25 @@
+import { useCallback } from 'react'
 import { TextField } from '@mui/material'
+import debounce from 'lodash/debounce'
 import { useSearchState } from 'store/generalStore'
 
 export default function AlbumsSearchField() {
-  const [search, setSearch] = useSearchState()
+  const [, setSearch] = useSearchState()
+
+  const debouncedSetSearch = useCallback(
+    debounce(value => {
+      setSearch(value)
+    }, 500),
+    []
+  )
+
   return (
     <TextField
       fullWidth
       label="Search"
       variant="outlined"
-      value={search}
       onChange={e => {
-        setSearch(e.target.value)
+        debouncedSetSearch(e.target.value)
       }}
     />
   )
