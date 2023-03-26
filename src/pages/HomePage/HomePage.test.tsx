d8b2ee25312c4441477e5a9674bab76de0c37aa8
Krystian Sz <sztadii@gmail.com>
2023-03-26 16:24:35 +0700

    Update and fix HomePage tests

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import HomePage from './HomePage'
 
-test('renders learn react link', () => {
-  render(<HomePage />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('HomePage', () => {
+  it('displays top albums heading', () => {
+    render(<HomePage />)
+    expect(screen.getByText('Top albums')).toBeVisible()
+  })
 })
