4f1c021e6deec36723c3b37df36990e28181fda7
Krystian Sz <sztadii@gmail.com>
2023-03-26 15:57:40 +0700

    Revise Home page UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,26 @@
 import React, { Suspense } from 'react'
-import logo from 'assets/images/logo.svg'
+import { Skeleton, Typography } from '@mui/material'
 import TopAlbums from './TopAlbums'
 import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <section className={styles.section}>
-        <img src={logo} className={styles.logo} alt="atom logo" />
+      <Typography variant="h3" mb={2}>
+        Top albums
+      </Typography>
 
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <Suspense fallback={<div>Loading...</div>}>
-          <TopAlbums />
-        </Suspense>
-      </section>
+      <Suspense
+        fallback={
+          <div>
+            {new Array(10).fill(null).map((_, index) => {
+              return <Skeleton key={index} width={300} variant="text" />
+            })}
+          </div>
+        }
+      >
+        <TopAlbums />
+      </Suspense>
     </div>
   )
 }
