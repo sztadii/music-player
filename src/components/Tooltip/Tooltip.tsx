d18d98d2e0690fd3a7aa0cd2a5bc3d2b5b70322d
Krystian Sz <sztadii@gmail.com>
2023-03-28 14:33:14 +0700

    Change Tooltip comments to JSDocs comments
    IDE will display those comments when we use the component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -3,20 +3,26 @@ import { ReactNode } from 'react'
 
 interface TooltipProps {
   title: string
+  /**
+   * In some cases, we may want to disable the tooltip,
+   * but MUITooltip doesn't provide an explicit way to do so.
+   */
   disabled?: boolean
-  // In some cases using the PortalAPI may cause overlapping with sticky elements,
-  // and in these instances, it's best to avoid using it.
-  // However, in most cases, the PortalAPI is very useful,
-  // which is why we enable portal API by default and disabled only in specific situations.
+  /**
+   * In some cases using the PortalAPI may cause overlapping with sticky elements,
+   * and in these instances, it's best to avoid using it.
+   * However, in most cases, the PortalAPI is very useful,
+   * which is why we enable portal API by default and disabled only in specific situations.
+   */
   disablePortal?: boolean
+  /**
+   * This wrapper was created because
+   * MUITooltip requires its children to be wrapped in a 'span' or
+   * 'div' element for the tooltip to display correctly.
+   */
   children: ReactNode
 }
 
-// This wrapper was created because
-// MUITooltip requires its children to be wrapped in a 'span' or
-// 'div' element for the tooltip to display correctly.
-// In some cases, we may want to disable the tooltip,
-// but MUITooltip doesn't provide an explicit way to do so.
 export default function Tooltip(props: TooltipProps) {
   const { children, disabled, title, disablePortal } = props
 
